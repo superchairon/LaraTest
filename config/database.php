fa7e2588a38ee28975fd3eb42fc7412e228f6d52
Jorge Rodriguez <jorge@Jorges-MacBookPro.local>
2019-08-04 22:10:16 -0500

    Changed database usage of environment variables

--- config/database.php
+++ config/database.php
@@ -2,13 +2,13 @@
 
 use Illuminate\Support\Str;
 
-if (getenv('LARATEST_MONGO_SERVICE_HOST')) {
-    putenv('MONGO_URL=mongodb://' . env("LARATEST_MONGO_SERVICE_HOST") . '/laratest'); // Remove leading slash from path
-}
-
-if (getenv('LARATEST_POSTGRES_SERVICE_HOST')) {
-    putenv('DB_HOST=' .  env("LARATEST_POSTGRES_SERVICE_HOST"));
-}
+//if (getenv('LARATEST_MONGO_SERVICE_HOST')) {
+//    putenv('MONGO_URL=mongodb://' . env("LARATEST_MONGO_SERVICE_HOST") . '/laratest'); // Remove leading slash from path
+//}
+//
+//if (getenv('LARATEST_POSTGRES_SERVICE_HOST')) {
+//    putenv('DB_HOST=' .  env("LARATEST_POSTGRES_SERVICE_HOST"));
+//}
 
 
 return [
